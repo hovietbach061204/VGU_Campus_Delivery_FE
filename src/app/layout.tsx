7f3f7b1ca29567841d44bb10f2cceff5b39752ea
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2025-03-24 09:02:45 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import '@/styles/globals.css';
+
+import { PropsWithChildren } from 'react';
+import { LanguageProvider } from '@inlang/paraglide-next';
+
+import { Footer } from '@/components/footer';
+import { Navbar } from '@/components/navbar/navbar';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeSwitcher } from '@/components/theme-switcher';
+import { Toaster } from '@/components/ui/toaster';
+import { fonts } from '@/lib/fonts';
+import { cn } from '@/lib/utils';
+import { languageTag } from '@/paraglide/runtime.js';
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <LanguageProvider>
+      <html lang={languageTag()} suppressHydrationWarning>
+        <body className={cn('min-h-screen font-sans', fonts)}>
+          <ThemeProvider attribute="class">
+            <Navbar />
+            {children}
+            <ThemeSwitcher className="absolute bottom-5 right-5 z-10" />
+            <Footer />
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </LanguageProvider>
+  );
+};
+
+export default RootLayout;
---
+++ src/lib/constant.ts
@@ -0,0 +1,18 @@
+import * as m from '@/paraglide/messages';
+
+export const siteConfig = {
+  title: m.meta_title,
+  description: m.meta_description,
+  keywords: () => [
+    m.meta_keyword_nextjs(),
+    m.meta_keyword_react(),
+    m.meta_keyword_nextjs_starter(),
+    m.meta_keyword_nextjs_boilerplate(),
+    m.meta_keyword_starter_template(),
+    m.meta_keyword_tailwindcss(),
+    m.meta_keyword_typescript(),
+    m.meta_keyword_shadcn_ui(),
+    m.meta_keyword_next_auth(),
+    m.meta_keyword_prisma(),
+  ],
+};
